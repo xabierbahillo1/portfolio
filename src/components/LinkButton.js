da9b54c55c4bff8c12c052d16d870921c12f966d
Xabier <xabierbahillo1@gmail.com>
2026-01-19 19:37:28 +0100

    feat: Mejoras esteticas en botones y header

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -1,12 +1,10 @@
-import React from "react";
-
 const LinkButton = ({ href, children }) => {
   return (
     <a
       target="_blank"
       rel="noopener noreferrer"
       href={href}
-      className="inline-flex bg-gray-100 text-gray-800 border-gray-300 items-center justify-center gap-2 px-3 py-2 space-x-2 text-base transition dark:text-white dark:bg-gray-800 border dark:border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black"
+      className="inline-flex bg-gray-100 text-gray-800 border-gray-300 items-center justify-center gap-2 px-3 py-2 space-x-2 text-base transition-all duration-300 dark:text-white dark:bg-gray-800 border dark:border-gray-600 focus-visible:ring-yellow-500/80 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black hover:scale-105 hover:shadow-lg"
     >
       {children}
     </a>
